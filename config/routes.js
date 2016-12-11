2e96b1f586b089c03548c922a1832209e1322a46
Sukhyun Baik <neonagx@gmail.com>
2016-12-10 21:05:50 -0800

    adds restful routes to professional event routes

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ var express = require('express'),
   methodOverride = require('method-override')
 
 var usersController = require('../controllers/users')
+var professionalsController = require('../controllers/professionals')
 var token = require('./token_auth')
 
 router.route('/api/users')
@@ -12,5 +13,13 @@ router.route('/api/token')
   .post(token.create)
 router.route('/api/me')
   .get(token.authenticate, usersController.me)
+
+router.route('/professionals')
+  .get(professionalsController.getAll)
+  .post(professionalsController.createProfessional)
+router.route('/professionals/:id')
+  .get(professionalsController.getProfessional)
+  .patch(professionalsController.updateProfessional)
+  .delete(professionalsController.deleteProfessional)
 
 module.exports = router
